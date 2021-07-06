016ce858601bebcac88436a84a067e43db791ea8
zuofan <815479015@qq.com>
2021-07-06 10:10:40 +0800

    重置初始化

---
+++ src/main/java/org/b3log/solo/bolo/tool/PassSSL.java
@@ -0,0 +1,52 @@
+/*
+ * Bolo - A stable and beautiful blogging system based in Solo.
+ * Copyright (c) 2020, https://github.com/adlered
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.b3log.solo.bolo.tool;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+public class PassSSL {
+
+    /**
+     * 设置可访问HTTPS
+     *
+     * @return null
+     */
+    public CloseableHttpClient createSSLClientDefault() {
+        try {
+            //信任所有
+            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, (chain, authType) -> true).build();
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+            return HttpClients
+                    .custom()
+                    .setSSLSocketFactory(sslsf)
+                    .build();
+        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+            e.printStackTrace();
+        }
+        return HttpClients.createDefault();
+    }
+}
---
+++ src/main/java/org/b3log/solo/util/PluginUtil.java
@@ -0,0 +1,38 @@
+/*
+ * Bolo - A stable and beautiful blogging system based in Solo.
+ * Copyright (c) 2020, https://github.com/adlered
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.b3log.solo.util;
+
+import org.b3log.latke.ioc.BeanManager;
+import org.b3log.solo.repository.PluginRepository;
+
+/**
+ * Get Bolo Plugin status.
+ */
+public class PluginUtil {
+
+    final static BeanManager beanManager = BeanManager.getInstance();
+    final static PluginRepository pluginRepository = beanManager.getReference(PluginRepository.class);
+
+    public static boolean b3logPluginEnabled() {
+        try {
+            return pluginRepository.get("B3log支持插件_0.0.1").optString("status").equals("ENABLED");
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
